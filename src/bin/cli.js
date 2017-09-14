e07997615424861aad6987c7241267f8cf70dd2d
Chris Hitchcott <hitchcott@gmail.com>
2017-09-14 15:01:44 +0800

    babel register the CLI, parse json string or file,

--- src/bin/cli.js
+++ src/bin/cli.js
@@ -1,3 +1,6 @@
+/* eslint-disable global-require */
+/* eslint-disable import/no-dynamic-require */
+
 import program from 'commander';
 import fs from 'fs';
 import getInstalledPath from 'get-installed-path';
@@ -6,25 +9,41 @@ import Dijix from '../';
 
 const defaultConfig = `${process.env.HOME}/.dijixrc`;
 
-const autoDetectTypes = ['dijix-image', 'dijix-pdf'];
+const autoDetectTypes = ['dijix-image', 'dijix-pdf', 'dijix-attestation'];
+// const autoDetectTypes = ['dijix-image'];
+
+function parseConfig(str) {
+  let config;
+  try {
+    // try parsing file content
+    config = fs.existsSync(str) && JSON.parse(fs.readFileSync(str).toString());
+  } catch (e) { /* ignore */ }
+  if (!config) {
+    try {
+      // try parsing the string itself
+      config = JSON.parse(str);
+    } catch (e) { /* ignore */ }
+  }
+  return config || {};
+}
 
 program
   .version('0.0.1')
   .usage('create <type> <src>')
   .description('Creates a dijix object of <type> using <src> path and upload it to ipfs')
-  .option('-c, --config [path]', 'base config file (JSON path); defaults to ~/.dijixrc', defaultConfig)
-  .option('-o, --overrides [path]', 'action overrides file (JSON path)')
-  .option('-t, --types [npm_modules]', 'specify types to register (comma seperated npm module names)', (v) => {
-    v.split(',').map(npm => ({ npm }));
-  })
+  .option('-c, --config [string / path]', 'base config file (JSON string or path); defaults to ~/.dijixrc', defaultConfig)
+  .option('-o, --options [string / path]', 'action options file (JSON string or path)')
+  .option('-t, --types [npm_modules]', 'specify types to register (comma seperated npm module names)', s => (
+    s.split(',').map(npm => ({ npm }))
+  ))
+  .option('-p, --plugins [npm_modules]', 'specify plugins to register (comma seperated npm module names)', s => (
+    s.split(',').map(p => p)
+  ))
   .action(async (cmd, type, src) => {
-    // read the config file
-    let config = {};
-    let overrides = {};
-    try {
-      config = JSON.parse(fs.readFileSync(program.config).toString());
-      overrides = program.overrides && JSON.parse(fs.readFileSync(program.overrides).toString());
-    } catch (e) { /* do nothing */ }
+    // parse the options
+    const config = parseConfig(program.config);
+    const options = parseConfig(program.options);
+
     // auto-detect types
     const detectedTypes = (
       await Promise.all(autoDetectTypes.map(async (module) => {
@@ -45,15 +64,16 @@ program
         const typePath = path || await getInstalledPath(npm);
         const required = require(typePath);
         const DijixType = required.default || required;
-        return new DijixType(typeConfig);
+        const t = new DijixType(typeConfig);
+        return t;
       } catch (e) {
         throw new Error(`Could not find path: ${npm || path} - ${e}`);
       }
     }));
     // TODO register plugins...
     const dijix = new Dijix({ ...config, types });
-    const obj = await dijix.create(type, { src, ...overrides });
-    console.log(JSON.stringify(obj, null, 2));
+    const obj = await dijix.create(type, { src, ...options });
+    process.stdout.write(JSON.stringify(obj, null, 2));
   });
 
 program.parse(process.argv);
--- src/bin/index.js
+++ src/bin/index.js
@@ -1,5 +1,9 @@
 #!/usr/bin/env node
 
-require('babel-register');
+process.on('unhandledRejection', console.error);
+
+require('babel-register')({
+  ignore: /node_modules\/(?!dijix-)/,
+});
 require('babel-polyfill');
 require('./cli.js');
